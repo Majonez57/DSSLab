bc7858f43db57168099eaec08961bf9997685a09
Adrian Vecina <avecinat@gmail.com>
2024-03-08 14:39:52 +0000

    up

--- readData.py
+++ readData.py
@@ -5,7 +5,7 @@ from time import sleep as zzz
 
 POLLSIZE = 100
 # accelerometer and magnetometer on any i2c pin
-sensor_pins={ "accel":3,"gyro":3,"sound":0, "button":2}
+sensor_pins={ "accel":3,"gyro":3, "accel2":2,"gyro2":2,"mag2":2,"sound":0, "button":2}
 sensors.set_pins(sensor_pins)
 
 timeinit = time.time()
@@ -20,16 +20,20 @@ while True:
 
     # acceleration in x,y,z axes
     ax,ay,az=sensors.accel.get_xyz()
+    ax2,ay2,az2=sensors.accel2.get_xyz()
     # magnitude of acceleration
     am = sensors.accel.get_magnitude()
+    am2 = sensors.accel.get_magnitude()
     # magnetic field strength in x,y,z axes
-    rx,ry,rz=sensors.gyro.get_xyz() 
+    rx,ry,rz=sensors.gyro.get_xyz()
+    rx2,ry2,rz2=sensors.gyro.get_xyz() 
     # magnitude of magnetic field
     rm = sensors.gyro.get_magnitude()
+    rm2 = sensors.gyro.get_magnitude()
 
     s =sensors.sound.get_level()
         
-    save_to_file('data.csv', [ax,ay,az,am,rx,ry,rz,rm,s])
+    save_to_file('data.csv', [ax,ay,az,am,rx,ry,rz,rm,ax2,ay2,az2,am2,rx2,ry2,rz2,rm2,s])
 
 
     print(f"Acceleration: {round(ax,2)},{round(ay,2)},{round(az,2)}, {round(am, 2)}")
